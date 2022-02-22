f5f00f4607cffbb57e99aad66f4f14c696791cbc
PC38\daw20 <rnietourena@safareyes.es>
2022-02-22 19:59:25 +0100

    falta const copia haciendo utilidades

--- src/main/java/utilidades/UtilidadesPersonaje.java
+++ src/main/java/utilidades/UtilidadesPersonaje.java
@@ -14,12 +14,14 @@ public class UtilidadesPersonaje {
     //Que recibiendo un personaje aumente su nivel en 1, actualizando
     //todas sus estadísticas según su escalabilidad.
     public Personaje levelUp(Personaje personaje){
-        personaje.setNivel(personaje.getNivel()+1);
-        personaje.setAtaqueBase(personaje.getAtaqueBase()+personaje.getEscalabilidad().getIncrementoDanioNivel()*personaje.getNivel());
-        personaje.setDefensaBase(personaje.getDefensaBase()+personaje.getEscalabilidad().getIncrementoDefensaNivel()*personaje.getNivel());
-        personaje.setManaBase(personaje.getManaBase()+personaje.getEscalabilidad().getIncrementoManaNivel()* personaje.getNivel());
-        personaje.setVidaBase(personaje.getVidaBase()+personaje.getEscalabilidad().getIncrementoSaludNivel()* personaje.getNivel());
-        return personaje;
+        if (personaje.getNivel()<18) {
+            personaje.setNivel(personaje.getNivel() + 1);
+            personaje.setAtaqueBase(personaje.getAtaqueBase() + personaje.getEscalabilidad().getIncrementoDanioNivel() * personaje.getNivel());
+            personaje.setDefensaBase(personaje.getDefensaBase() + personaje.getEscalabilidad().getIncrementoDefensaNivel() * personaje.getNivel());
+            personaje.setManaBase(personaje.getManaBase() + personaje.getEscalabilidad().getIncrementoManaNivel() * personaje.getNivel());
+            personaje.setVidaBase(personaje.getVidaBase() + personaje.getEscalabilidad().getIncrementoSaludNivel() * personaje.getNivel());
+        }
+            return personaje;
     }
 
     //Que a partir de un listado de personajes devuelve un mapa de los
@@ -29,7 +31,25 @@ public class UtilidadesPersonaje {
         return personajes.stream().collect(Collectors.groupingBy(p-> p.getRegion()));
     }
 
-    //
+    //A partir de una lista de personajes, devuelve el personaje más
+    //poderoso.
+    public Personaje getMasPoderoso(List<Personaje> personaje){
+        Personaje poderoso = new Personaje();
+        for (Personaje perso :personaje){
+            while(perso.getNivel()<18){
+                levelUp(perso);
+            }
+            if (poderoso.getNivel()==null){
+                poderoso = perso;
+            }
+            if((perso.getAtaque()+ perso.getDefensa()+perso.getMana()+perso.getVida())>
+               (perso.getAtaque())+poderoso.getDefensa()+ poderoso.getMana()+ poderoso.getVida()){
+                poderoso = perso;
+            }
+        }
+        return poderoso;
+    }
+
 
 
 }
---
+++ src/test/java/pruebas.java
@@ -0,0 +1,8 @@
+import modelos.Personaje;
+
+public class pruebas {
+    public static void main(String[] args) {
+        Personaje perso = new Personaje();
+        System.out.println(perso);
+    }
+}
